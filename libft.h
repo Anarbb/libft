f339fd452edbb74de973371cba2972d0ba57f887
Anas Arbaoui <aarbaoui@e2r7p10.1337.ma>
2022-10-05 13:30:26 +0100

    OK.

--- libft.h
+++ libft.h
@@ -13,6 +13,6 @@ int		ft_toupper(int c);
 int		ft_tolower(int c);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
-int	ft_strncmp(const char *s1, const char *s2, size_t n);
-
+/*int		ft_strncmp(const char *s1, const char *s2, size_t n);
+*/
 #endif
