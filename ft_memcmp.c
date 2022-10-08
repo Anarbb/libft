e6b5395abbb346a1c7713c0d46923530febda54f
Anas Arbaoui <aarbaoui@e1r5p3.1337.ma>
2022-10-08 13:57:00 +0100

    OK.

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -1,23 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aarbaoui <aarbaoui@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/05 19:16:55 by aarbaoui          #+#    #+#             */
-/*   Updated: 2022/10/08 13:18:33 by aarbaoui         ###   ########.fr       */
+/*   Created: 2022/10/08 13:23:30 by aarbaoui          #+#    #+#             */
+/*   Updated: 2022/10/08 13:47:52 by aarbaoui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	*ft_memset(void *b, int c, int len)
-{
-	unsigned char	*p;
+#include "libft.h"
 
-	p = (unsigned char *) b;
-	while (0 < len--)
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	unsigned char	*str1;
+	unsigned char	*str2;
+
+	while (n--)
 	{
-		*p++ = c;
+		str1 = (unsigned char *) s1++;
+		str2 = (unsigned char *) s2++;
+		if (str1 != str2)
+			return (s1 - s2);
+		if (str1 && str2 == '\0')
+			return (0);
 	}
-	return (b);
+	return (0);
 }
