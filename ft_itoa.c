567d4d2d689a750e073e0630409215865d75e393
Anas <anas@arbaoui.me>
2022-10-14 12:55:21 +0100

    OK.

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,39 +6,83 @@
 /*   By: aarbaoui <aarbaoui@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/14 10:08:56 by aarbaoui          #+#    #+#             */
-/*   Updated: 2022/10/14 10:29:21 by aarbaoui         ###   ########.fr       */
+/*   Updated: 2022/10/14 12:54:21 by aarbaoui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <string.h>
+#include <stdio.h>
 
 static int	ft_getintlen(int value)
 {
-  int l;
+	int	l;
+	int	neg;
 
-  l = 1;
-  while (value > 9)
+	l = 1;
+	neg = 1;
+	if (value < 0)
+	{
+		value *= -1;
+		neg = -1;
+	}
+	while (value > 9)
 	{
 		l++;
 		value /= 10;
 	}
-  return (l);
+	if (neg == -1)
+	{
+		return (l + 1);
+	}
+	return (l);
+}
+
+static int	ft_isneg(int n)
+{
+	if (n < 0)
+		return (-1);
+	return (1);
+}
+
+static char	*ft_strcpy(char *dest, const char *src)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = src[i];
+	return (dest);
 }
 
 char	*ft_itoa(int n)
 {
-	int	i;
+	size_t	len;
 	char	*instr;
+	int		neg;
 
-	i = ft_getintlen(n);
-	instr = (char *)malloc(sizeof(char) * i);
+	neg = ft_isneg(n);
+	len = ft_getintlen(n);
+	instr = (char *)malloc((sizeof(char) * len) + 1);
+	if (n == -2147483648)
+		return (ft_strcpy(instr, "-2147483648"));
 	if (!instr)
 		return (NULL);
+	if (neg == -1)
+		n *= -1;
+	instr[len--] = 0;
+	if (n == 0)
+		instr[len--] = 48;
 	while (n)
 	{
-		printf("%d\n", n%10);
+		instr[len--] = ((n % 10) + 48);
 		n /= 10;
 	}
-	return 0;
+	if (neg == -1)
+		instr[len] = '-';
+	return (instr);
 }
-
