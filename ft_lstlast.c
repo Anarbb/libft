cb209bac70198edaadcc357ed8c7feb245ae87be
Anas <anas@arbaoui.me>
2022-10-16 13:03:05 +0100

    OK.

---
+++ ft_lstlast.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstlast.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aarbaoui <aarbaoui@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/16 12:14:39 by aarbaoui          #+#    #+#             */
+/*   Updated: 2022/10/16 13:03:00 by aarbaoui         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstlast(t_list *lst)
+{
+	struct s_list *ptr;
+	
+	ptr = lst;
+	if (!ptr)
+		return (NULL);
+	while (ptr->next != NULL)
+		ptr = ptr->next;
+	return (ptr);
+}
